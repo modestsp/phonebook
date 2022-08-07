e987032ea845cf94184e6162a730026962874c79
ss <csperichon@gmail.com>
2022-08-07 18:04:27 -0300

    Info route

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ app.use(express.static('build'))
 morgan.token('body', function (req, res) {
   return JSON.stringify(req.body)
 })
+// app.use(morgan(':body :method :url :status :res[content-length] - :response-time ms'))
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if(error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
 
 
 // Fetching all persons
@@ -22,19 +37,36 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-
-// Fetching individual person
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+// Info route
+app.get('/api/info', (request, response) => {
+  Person.find({}).then((persons) => {
+    response.send(`<div>
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`)
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+// Fetching individual person
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if(person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
 
-  response.status(204).end()
+
+// Deleting a contact
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // Creating a new contact
@@ -46,6 +78,7 @@ app.post('/api/persons', (request, response) => {
       error: 'name and number are required'
     })
   }
+
   // Repeated name
   // persons.forEach(person => {
   //   if(person.name === body.name) {
@@ -66,6 +99,24 @@ app.post('/api/persons', (request, response) => {
 })
 
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person
+    .findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server Running on port ${PORT}`)
