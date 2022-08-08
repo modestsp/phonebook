66ab7df34e5a01a804f55a83ae82ddd243c11ec2
ss <csperichon@gmail.com>
2022-08-08 12:35:17 -0300

    Error handlers

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const errorHandler = (error, request, response, next) => {
 
   if(error.name === 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  } else if(error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -31,10 +33,12 @@ const unknownEndpoint = (request, response) => {
 
 
 // Fetching all persons
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then((persons) => {
-    response.json(persons)
-  })
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
 // Info route
@@ -70,14 +74,9 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 // Creating a new contact
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if(!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'name and number are required'
-    })
-  }
 
   // Repeated name
   // persons.forEach(person => {
@@ -93,26 +92,27 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
-
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
+  const {name, number} = request.body
 
   Person
-    .findByIdAndUpdate(request.params.id, person, { new: true })
-    .then(updatedPerson => {
-      response.json(updatedPerson)
-    })
-    .catch(error => next(error))
+      .findByIdAndUpdate(
+        request.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+        )
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
